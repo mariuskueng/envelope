03047cf041aadf71f521a0db7130d568c672363e
Marius Küng <mk@mariuskueng.ch>
2013-10-16 20:26:48 +0200

    added jquery fallback, cleanup

--- main.js
+++ main.js
@@ -8,30 +8,43 @@ http://designshack.net/articles/css/code-a-simple-folded-corner-effect-with-css/
 $(function(){
 
     $('#arrow_click').on('click', function(){
-        animate_flip($(this));
+        animate_flip($(this), $('.flip'));
 
+        // Modernizr.csstransitions = false;
     });
 });
 
-function animate_flip(el){
+function animate_flip(arrow_click, flip){
     /*
     1. Lasche verlängern
     2. Band verkürzen/"herunterziehen"
     3. Schriftzug auf gleicher Position halten/animieren damit es nicht verschoben wird
     */
 
-    var $el = el;
-    $el.css('-webkit-animation', 'none');
-    $el.css('border-top-width', $(window).height());
-    $('.flip').css('top', $(window).height());
-    $('.flip').addClass('animated_background_position');
+    if(Modernizr.csstransitions){
+        flip.css('-webkit-transition', 'all 1s ease-in-out');
+        arrow_click.css('-webkit-animation', 'none');
+        arrow_click.css('border-top-width', $(window).height());
+        flip.css('top', $(window).height());
+        flip.addClass('animated_background_position');
+    }
+    else{
+        arrow_click.animate({'border-top-width': $(window).height()}, 500);
+        flip.animate({'background-position': '50% -77.5%'}, 750);
+        flip.animate({'top': $(window).height()}, 250);
+    }
     var timeout = window.setTimeout(animate_sides, 1000);
 }
 
 function animate_sides(){
 
-    $('.side.left').addClass('animated_left');
-    $('.side.right').addClass('animated_right');
-    //$('.flip').css('backgroundPosition', "50% -100%");
+    if(Modernizr.csstransitions){
+        $('.side.left').addClass('animated_left');
+        $('.side.right').addClass('animated_right');
+    }
+    else{
+        $('.side.left').animate({'left': '-100%'});
+        $('.side.right').animate({'right': '-100%'});
+    }
 
 }
